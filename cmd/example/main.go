bc68985b671248a5edbb5b35f72acbe9e13a2075
iyan subdiana <subdiana.project@gmail.com>
2025-10-23 14:50:59 +0700

    add example for use

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/i-sub135/i-sub-orm/internal/expr"
+	"github.com/i-sub135/i-sub-orm/pkg/orm"
+	_ "github.com/mattn/go-sqlite3" // Import sqlite3 driver
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := orm.Open("sqlite3", "file:test.db?cache=shared&mode=memory")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Example chaining with expression-style WHEREs
+	err = db.Table("users").
+		Select("*").
+		Where(expr.Eq{"status": "active", "role": "admin"}).
+		Where(expr.Gt{"age": 18}).
+		Get(nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/i-sub135/i-sub-orm
 
 go 1.22.2
 
-require github.com/mattn/go-sqlite3 v1.14.32 // indirect
+require github.com/mattn/go-sqlite3 v1.14.32
