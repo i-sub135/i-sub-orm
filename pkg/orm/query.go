b5d465c31b352705adc1b9b2fe7fb0b4a00bd126
iyan subdiana <subdiana.project@gmail.com>
2025-10-27 15:52:52 +0700

    implement scan row for final builder

--- pkg/orm/query.go
+++ pkg/orm/query.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/i-sub135/i-sub-orm/internal/expr"
+	"github.com/i-sub135/i-sub-orm/internal/utils"
 )
 
 type Query struct {
@@ -38,11 +39,18 @@ func (q *Query) Where(cond any, args ...any) *Query {
 
 func (q *Query) Build() string {
 	sql := "SELECT "
+
+	// Handle fields
 	if len(q.fields) == 0 {
+		sql += "*"
 	} else {
 		sql += strings.Join(q.fields, ", ")
 	}
 
+	// Add FROM clause
+	sql += " FROM " + q.table
+
+	// Add WHERE clause
 	if len(q.where) > 0 {
 		sql += " WHERE " + strings.Join(q.where, " AND ")
 	}
@@ -52,11 +60,15 @@ func (q *Query) Build() string {
 
 func (q *Query) Get(dest any) error {
 	query := q.Build()
+
+	// Rebind placeholders for the specific driver
+	query = utils.RebindPlaceholder(query, q.executor.driver)
+
 	fmt.Println("Executing:", query, "Args:", q.args)
 	rows, err := q.executor.exec.Query(query, q.args...)
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
-	return nil
+	return utils.ScanRows(rows, dest)
 }
