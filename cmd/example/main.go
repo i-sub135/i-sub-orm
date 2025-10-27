b57c6958a52e9eb8b6d391b074fe9bc93658358a
iyan subdiana <subdiana.project@gmail.com>
2025-10-27 15:53:27 +0700

    create test code for use

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"log"
+	"time"
 
-	"github.com/i-sub135/i-sub-orm/internal/expr"
+	"github.com/i-sub135/i-sub-orm/internal/driver"
+	// "github.com/i-sub135/i-sub-orm/internal/expr"
 	"github.com/i-sub135/i-sub-orm/pkg/orm"
-	_ "github.com/mattn/go-sqlite3" // Import sqlite3 driver
+	_ "github.com/lib/pq" // Import postgres driver
+	// _ "github.com/mattn/go-sqlite3" // Import sqlite3 driver
 )
 
+// User represents the users table in the database
+type User struct {
+	ID        string    `db:"id"`
+	Email     string    `db:"email"`
+	Password  string    `db:"password"`
+	Name      string    `db:"name"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+// DATABASE_URL="postgresql://tracking_user:tracking_pass@localhost:5432/tracking_db"
 func main() {
-	db, err := orm.Open("sqlite3", "file:test.db?cache=shared&mode=memory")
+	db, err := orm.Open(driver.Postgres.String(), "host=localhost port=5432 user=tracking_user password=tracking_pass dbname=tracking_db sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// Example chaining with expression-style WHEREs
+	// Example: Get all users
+	var users []User
 	err = db.Table("users").
 		Select("*").
-		Where(expr.Eq{"status": "active", "role": "admin"}).
-		Where(expr.Gt{"age": 18}).
-		Get(nil)
+		Get(&users)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("Found %d users", len(users))
+	log.Printf("%+v", users)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/i-sub135/i-sub-orm
 go 1.22.2
 
 require github.com/mattn/go-sqlite3 v1.14.32
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
